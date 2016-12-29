8e6f6618eb3715b96b93ae77599ae3e95da720e8
Angelo <angelooparah@gmail.com>
2016-12-29 19:07:20 +0000

    added instance vars to ship class plus getters and setters

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -5,6 +5,18 @@ public abstract class Ship
 	/** measures the length of the ship */
 	private int length;
 
+	/** indicates the vertical position of the front of the ship */
+	private int bowRow;
+
+	/** indicates the horizontal position of the front of the ship */
+	private int bowColumn;
+
+	/** set to true to indicate that the ship occupies a single row */
+	private boolean horizontal;
+
+	/** indicates what part of the ship has been hit */
+	private boolean[] hit;
+
 	protected Ship(int length)
 	{
 		this.length = length;
@@ -13,5 +25,35 @@ public abstract class Ship
 	public int getLength()
 	{
 		return length;
+	}
+
+	public int getBowRow()
+	{
+		return bowRow;
+	}
+
+	public void setBowRow(int bowRow)
+	{
+		this.bowRow = bowRow;
+	}
+
+	public int getBowColumn()
+	{
+		return bowColumn;
+	}
+
+	public void setBowColumn(int bowColumn)
+	{
+		this.bowColumn = bowColumn;
+	}
+
+	public boolean isHorizontal()
+	{
+		return horizontal;
+	}
+
+	public void setHorizontal(boolean horizontal)
+	{
+		this.horizontal = horizontal;
 	}
 }
