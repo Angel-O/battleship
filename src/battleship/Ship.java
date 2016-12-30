3debcfa8ae0f813f1303252cd22f0c36b579c786
Angelo <angelooparah@gmail.com>
2016-12-30 00:28:40 +0000

    completing ships class and adding tests to check shooting at ships

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -21,12 +21,13 @@ public abstract class Ship
 	{
 		assert length > 0 : "ship length cannot be negative";
 		this.length = length;
+		hit = new boolean[length];
 	}
 
 	/**
 	 * Returns the type of the ship
 	 *
-	 * @return the type of the ship
+	 * @return a string indicating the type of the ship
 	 */
 	public abstract String getShipType();
 
@@ -35,16 +36,59 @@ public abstract class Ship
 	 * hasn't been sunk, mark that part of the ship as hit (in the hit array,
 	 * index 0 indicates the bow) and return true, otherwise return false.
 	 *
+	 * @param row
+	 *            represents the vertical coordinate to be hit
+	 * @param column
+	 *            represents the horizontal coordinate to be hit
 	 * @return {@code true} if the ship was hit, {@code false} otherwise
 	 */
-	public boolean shootAt(int row, int col)
+	public boolean shootAt(int row, int column)
 	{
+		if (!isSunk())
+		{
+			if (isHorizontal())
+			{
+				return tryShoot(getBowColumn(), column);
+			}
+			else
+			{
+				return tryShoot(getBowRow(), row);
+			}
+		}
 		return false;
 	}
 
 	/**
-	 * returns true if every part of the ship has been hit (and therefore the
-	 * ship is sunk) false otherwise
+	 * Tries to hit the ship. Marks the part that was hit and returns true in
+	 * case of success, otherwise it returns false.
+	 *
+	 * @param bowCoordinate
+	 *            coordinate of the bow. If the ship is horizontal the bow
+	 *            column coordinate should be used
+	 * @param targetCoordinate
+	 *            coordinate targeted by the hit. If the ship is horizontal the
+	 *            target column coordinate should be used
+	 * @return {@code true} if it's a hit, {@code false} it if's a miss
+	 */
+	private boolean tryShoot(int bowCoordinate, int targetCoordinate)
+	{
+		// Check if the ship was hit. Add the length to the relevant coordinate
+		// and subtract one since the ship will stretch from the bow to length -
+		// 1 at most.
+		if (bowCoordinate + length - 1 >= targetCoordinate)
+		{
+			// mark the hit point
+			hit[targetCoordinate - bowCoordinate] = true;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Indicates if the ship has been sunk.
+	 *
+	 * @return {@code true} if every part of the ship has been hit {@code false}
+	 *         otherwise.
 	 */
 	public boolean isSunk()
 	{
@@ -60,36 +104,77 @@ public abstract class Ship
 		return true;
 	}
 
+	/**
+	 * Returns the length of the ship
+	 *
+	 * @return the length of the ship
+	 */
 	public int getLength()
 	{
 		return length;
 	}
 
+	/**
+	 * Returns the vertical coordinate of the bow
+	 *
+	 * @return the vertical coordinate of the bow
+	 */
 	public int getBowRow()
 	{
 		return bowRow;
 	}
 
+	/**
+	 * Sets the vertical coordinate of the bow
+	 *
+	 * @param bowRow
+	 *            value to set the vertical coordinate to
+	 */
 	public void setBowRow(int bowRow)
 	{
 		this.bowRow = bowRow;
 	}
 
+	/**
+	 * Returns the horizontal coordinate of the bow
+	 *
+	 * @return the horizontal coordinate of the bow
+	 */
 	public int getBowColumn()
 	{
 		return bowColumn;
 	}
 
+	/**
+	 * Sets the horizontal coordinate of the bow
+	 *
+	 * @param bowColumn
+	 *            value to set the vertical coordinate to
+	 */
 	public void setBowColumn(int bowColumn)
 	{
 		this.bowColumn = bowColumn;
 	}
 
+	/**
+	 * Refers the orientation of the ship
+	 *
+	 * @return {@code true} if the ship is positioned horizontally,
+	 *         {@code false} otherwise
+	 */
 	public boolean isHorizontal()
 	{
 		return horizontal;
 	}
 
+	/**
+	 * Sets the orientation of the ship
+	 *
+	 * @param horizontal
+	 *            {@code boolean} flag indicating the horizontal orientation of
+	 *            the ship. A {@code true} value sets it to horizontal, a
+	 *            {@code false} value sets it to vertical.
+	 */
 	public void setHorizontal(boolean horizontal)
 	{
 		this.horizontal = horizontal;
@@ -98,7 +183,8 @@ public abstract class Ship
 	/**
 	 * Indicates whether or not the ship is real
 	 *
-	 * @return {@code true} if it's a real ship, {@code false} otherwise
+	 * @return {@code true} (default value) if it's a real ship, {@code false}
+	 *         otherwise.
 	 */
 	public boolean isRealShip()
 	{
--- src/battleship/ShipTest.java
+++ src/battleship/ShipTest.java
@@ -4,6 +4,7 @@
 package battleship;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -49,7 +50,7 @@ public class ShipTest
 	{}
 
 	@Test(timeout = DEFAULT_TIMEOUT)
-	public void test_should_return_battleship_correct_lenght()
+	public void test_should_return_battleship_correct_length()
 	{
 		Ship ship = new Battleship();
 		int expected = 4;
@@ -58,7 +59,7 @@ public class ShipTest
 	}
 
 	@Test(timeout = DEFAULT_TIMEOUT)
-	public void test_should_return_cruiser_correct_lenght()
+	public void test_should_return_cruiser_correct_length()
 	{
 		Ship ship = new Cruiser();
 		int expected = 3;
@@ -67,7 +68,7 @@ public class ShipTest
 	}
 
 	@Test(timeout = DEFAULT_TIMEOUT)
-	public void test_should_return_destroyer_correct_lenght()
+	public void test_should_return_destroyer_correct_length()
 	{
 		Ship ship = new Destroyer();
 		int expected = 2;
@@ -76,7 +77,7 @@ public class ShipTest
 	}
 
 	@Test(timeout = DEFAULT_TIMEOUT)
-	public void test_should_return_submarine_correct_lenght()
+	public void test_should_return_submarine_correct_length()
 	{
 		Ship ship = new Submarine();
 		int expected = 1;
@@ -85,11 +86,98 @@ public class ShipTest
 	}
 
 	@Test(timeout = DEFAULT_TIMEOUT)
-	public void test_should_return_empty_sea_dummy_lenght()
+	public void test_should_return_empty_sea_dummy_length()
 	{
 		Ship ship = new EmptySea();
 		int expected = 1;
 		int actual = ship.getLength();
 		assertEquals("verifying dummy length of empty sea", expected, actual);
+	}
+
+	@Test(timeout = DEFAULT_TIMEOUT)
+	public void test_horizontal_ships_should_be_sunk_when_shots_are_fired_across_their_length()
+	{
+		// set the ships bow at position (1,1) and align them horizontally
+		Ship battleship = new Battleship();
+		battleship.setBowRow(1);
+		battleship.setBowColumn(1);
+		battleship.setHorizontal(true);
+
+		Ship destroyer = new Destroyer();
+		destroyer.setBowRow(1);
+		destroyer.setBowColumn(1);
+		destroyer.setHorizontal(true);
+
+		Ship cruiser = new Cruiser();
+		cruiser.setBowRow(1);
+		cruiser.setBowColumn(1);
+		cruiser.setHorizontal(true);
+
+		Ship submarine = new Submarine();
+		submarine.setBowRow(1);
+		submarine.setBowColumn(1);
+		submarine.setHorizontal(true);
+
+		// shoot each part along the length of the ship
+		shootHorizontallyAcrossShipLength(battleship);
+		shootHorizontallyAcrossShipLength(destroyer);
+		shootHorizontallyAcrossShipLength(cruiser);
+		shootHorizontallyAcrossShipLength(submarine);
+
+		// verify that the ships have been actually sunk
+		assertTrue("sinking a horizontal battleship", battleship.isSunk());
+		assertTrue("Sinking a horizontal destroyer", destroyer.isSunk());
+		assertTrue("sinking a horizontal cruiser", cruiser.isSunk());
+		assertTrue("Sinking a horizontal submarine", submarine.isSunk());
+	}
+
+	@Test(timeout = DEFAULT_TIMEOUT)
+	public void test_vertical_ships_should_be_sunk_when_shots_are_fired_across_their_length()
+	{
+		// set the ships bow at position (1,1); by default they will be aligned
+		// vertically
+		Ship battleship = new Battleship();
+		battleship.setBowRow(1);
+		battleship.setBowColumn(1);
+
+		Ship destroyer = new Destroyer();
+		destroyer.setBowRow(1);
+		destroyer.setBowColumn(1);
+
+		Ship cruiser = new Cruiser();
+		cruiser.setBowRow(1);
+		cruiser.setBowColumn(1);
+
+		Ship submarine = new Submarine();
+		submarine.setBowRow(1);
+		submarine.setBowColumn(1);
+
+		// shoot each part along the length of the ship
+		shootVerticallyAcrossShipLength(battleship);
+		shootVerticallyAcrossShipLength(destroyer);
+		shootVerticallyAcrossShipLength(cruiser);
+		shootVerticallyAcrossShipLength(submarine);
+
+		// verify that the ships have been actually sunk
+		assertTrue("sinking a vertical battleship", battleship.isSunk());
+		assertTrue("Sinking a vertical destroyer", destroyer.isSunk());
+		assertTrue("sinking a vertical cruiser", cruiser.isSunk());
+		assertTrue("Sinking a vertical submarine", submarine.isSunk());
+	}
+
+	private void shootHorizontallyAcrossShipLength(Ship ship)
+	{
+		for (int i = ship.getBowColumn(); i < ship.getBowColumn() + ship.getLength(); i++)
+		{
+			ship.shootAt(ship.getBowRow(), i);
+		}
+	}
+
+	private void shootVerticallyAcrossShipLength(Ship ship)
+	{
+		for (int i = ship.getBowRow(); i < ship.getBowRow() + ship.getLength(); i++)
+		{
+			ship.shootAt(i, ship.getBowColumn());
+		}
 	}
 }
